89df5d8825f74fe1616d01f048613d103dd91424
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-06-23 15:47:40 +0300

    Add website next

---
+++ website/app/not-found.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/errorPage.module.css';
+
+export default function NotFound(){
+    return(
+        <div><div className={styles.page}><h1>404</h1></div></div>
+    )
+}
