15afb43079b3c09165f71e0d4c25d86d7522885a
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-06-23 14:40:17 +0300

    Update routes.js

--- Server/api/Test/routes.js
+++ Server/api/Test/routes.js
@@ -10,9 +10,5 @@ router.get('/', async (req, res) => {
         return res.status(200).json({ message: "ok" });
     } catch (error) {
         return res.status(500).json({ error: 'Internal Server Error: ' + error.message });
-    } finally {
-        if (client) {
-            client.release(); 
-        }
     }
 });
