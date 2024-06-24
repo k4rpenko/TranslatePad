2af270dc428a523034e90527d04666be954c4619
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-06-24 20:47:02 +0300

    Update Design

--- Client/Client/Authorization.Designer.cs
+++ Client/Client/Authorization.Designer.cs
@@ -54,7 +54,7 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(227, 63);
             this.tabControl1.Name = "tabControl1";
-            this.tabControl1.Padding = new System.Drawing.Point(98, 3);
+            this.tabControl1.Padding = new System.Drawing.Point(120, 15);
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(554, 419);
             this.tabControl1.TabIndex = 0;
@@ -67,10 +67,10 @@
             this.tabPage1.Controls.Add(this.Sign_up_pass2);
             this.tabPage1.Controls.Add(this.Sign_up_email);
             this.tabPage1.Controls.Add(this.Sign_up_button);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 46);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(546, 393);
+            this.tabPage1.Size = new System.Drawing.Size(546, 369);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Sign up";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -143,10 +143,10 @@
             this.tabPage2.Controls.Add(this.Sign_in_button);
             this.tabPage2.Controls.Add(this.Sign_in_pass);
             this.tabPage2.Controls.Add(this.Sign_in_email);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 46);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(546, 393);
+            this.tabPage2.Size = new System.Drawing.Size(546, 369);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Sign in";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -206,7 +206,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 615);
+            this.ClientSize = new System.Drawing.Size(1004, 611);
             this.Controls.Add(this.tabControl1);
             this.Name = "Authorization";
             this.Text = "Authorization";
