864f5ffaecca6f920312b6187219379d3c98f110
Oksi598 <radovunoksana@gmail.com>
2024-06-25 01:06:31 +0300

    Desing Menu

--- Client/Client/Menu.cs
+++ Client/Client/Menu.cs
@@ -16,5 +16,40 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
