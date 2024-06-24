b1772f6ddb2f8613d540470b2499215f0ac41aaa
Dima1385 <Lozik1385@gmail.com>
2024-06-24 16:09:41 +0300

    Fix and update

--- Client/Client/Authorization.Designer.cs
+++ Client/Client/Authorization.Designer.cs
@@ -31,18 +31,18 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.Sign_up_pass = new System.Windows.Forms.TextBox();
             this.Sign_up_pass2 = new System.Windows.Forms.TextBox();
             this.Sign_up_email = new System.Windows.Forms.TextBox();
             this.Sign_up_button = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.Sign_in_button = new System.Windows.Forms.Button();
             this.Sign_in_pass = new System.Windows.Forms.TextBox();
             this.Sign_in_email = new System.Windows.Forms.TextBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -56,7 +56,7 @@
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.Padding = new System.Drawing.Point(66, 3);
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(365, 300);
+            this.tabControl1.Size = new System.Drawing.Size(488, 404);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -70,129 +70,135 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(357, 274);
+            this.tabPage1.Size = new System.Drawing.Size(480, 378);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(133, 140);
+            this.checkBox1.Location = new System.Drawing.Point(188, 147);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
             this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Show password";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Sign_up_pass
             // 
-            this.Sign_up_pass.Location = new System.Drawing.Point(133, 72);
+            this.Sign_up_pass.Location = new System.Drawing.Point(175, 81);
             this.Sign_up_pass.Name = "Sign_up_pass";
-            this.Sign_up_pass.Size = new System.Drawing.Size(100, 20);
+            this.Sign_up_pass.Size = new System.Drawing.Size(125, 20);
             this.Sign_up_pass.TabIndex = 3;
+            this.Sign_up_pass.UseSystemPasswordChar = true;
             this.Sign_up_pass.TextChanged += new System.EventHandler(this.Sign_up_pass_TextChanged);
             // 
             // Sign_up_pass2
             // 
-            this.Sign_up_pass2.Location = new System.Drawing.Point(133, 113);
+            this.Sign_up_pass2.Location = new System.Drawing.Point(175, 107);
             this.Sign_up_pass2.Name = "Sign_up_pass2";
-            this.Sign_up_pass2.Size = new System.Drawing.Size(100, 20);
+            this.Sign_up_pass2.Size = new System.Drawing.Size(125, 20);
             this.Sign_up_pass2.TabIndex = 2;
+            this.Sign_up_pass2.UseSystemPasswordChar = true;
             this.Sign_up_pass2.TextChanged += new System.EventHandler(this.Sign_up_pass2_TextChanged);
             // 
             // Sign_up_email
             // 
-            this.Sign_up_email.Location = new System.Drawing.Point(133, 33);
+            this.Sign_up_email.Location = new System.Drawing.Point(175, 37);
             this.Sign_up_email.Name = "Sign_up_email";
-            this.Sign_up_email.Size = new System.Drawing.Size(100, 20);
+            this.Sign_up_email.Size = new System.Drawing.Size(125, 20);
             this.Sign_up_email.TabIndex = 1;
             this.Sign_up_email.TextChanged += new System.EventHandler(this.Sign_up_email_TextChanged);
             // 
             // Sign_up_button
             // 
-            this.Sign_up_button.Location = new System.Drawing.Point(138, 214);
+            this.Sign_up_button.Location = new System.Drawing.Point(188, 205);
             this.Sign_up_button.Name = "Sign_up_button";
-            this.Sign_up_button.Size = new System.Drawing.Size(75, 23);
+            this.Sign_up_button.Size = new System.Drawing.Size(89, 35);
             this.Sign_up_button.TabIndex = 0;
-            this.Sign_up_button.Text = "button1";
+            this.Sign_up_button.Text = "Sign up";
             this.Sign_up_button.UseVisualStyleBackColor = true;
             this.Sign_up_button.Click += new System.EventHandler(this.Sign_up_button_Click);
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.checkBox2);
             this.tabPage2.Controls.Add(this.label2);
             this.tabPage2.Controls.Add(this.Sign_in_button);
             this.tabPage2.Controls.Add(this.Sign_in_pass);
             this.tabPage2.Controls.Add(this.Sign_in_email);
-            this.tabPage2.Controls.Add(this.checkBox2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(357, 274);
+            this.tabPage2.Size = new System.Drawing.Size(480, 378);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(188, 107);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(101, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Show password";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Sign_in_button
             // 
-            this.Sign_in_button.Location = new System.Drawing.Point(138, 162);
+            this.Sign_in_button.Location = new System.Drawing.Point(188, 205);
             this.Sign_in_button.Name = "Sign_in_button";
-            this.Sign_in_button.Size = new System.Drawing.Size(75, 23);
+            this.Sign_in_button.Size = new System.Drawing.Size(89, 35);
             this.Sign_in_button.TabIndex = 3;
-            this.Sign_in_button.Text = "button2";
+            this.Sign_in_button.Text = "Sign in";
             this.Sign_in_button.UseVisualStyleBackColor = true;
             this.Sign_in_button.Click += new System.EventHandler(this.Sign_in_button_Click);
             // 
             // Sign_in_pass
             // 
-            this.Sign_in_pass.Location = new System.Drawing.Point(128, 82);
+            this.Sign_in_pass.Location = new System.Drawing.Point(175, 81);
             this.Sign_in_pass.Name = "Sign_in_pass";
-            this.Sign_in_pass.Size = new System.Drawing.Size(100, 20);
+            this.Sign_in_pass.Size = new System.Drawing.Size(125, 20);
             this.Sign_in_pass.TabIndex = 2;
+            this.Sign_in_pass.UseSystemPasswordChar = true;
             this.Sign_in_pass.TextChanged += new System.EventHandler(this.Sign_in_pass_TextChanged);
             // 
             // Sign_in_email
             // 
-            this.Sign_in_email.Location = new System.Drawing.Point(128, 41);
+            this.Sign_in_email.Location = new System.Drawing.Point(175, 37);
             this.Sign_in_email.Name = "Sign_in_email";
-            this.Sign_in_email.Size = new System.Drawing.Size(100, 20);
+            this.Sign_in_email.Size = new System.Drawing.Size(125, 20);
             this.Sign_in_email.TabIndex = 1;
             this.Sign_in_email.TextChanged += new System.EventHandler(this.Sign_in_email_TextChanged);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(128, 116);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 0;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(133, 164);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(125, 136);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
             // 
             // Authorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1004, 615);
             this.Controls.Add(this.tabControl1);
             this.Name = "Authorization";
             this.Text = "Authorization";
@@ -218,8 +224,8 @@
         private System.Windows.Forms.Button Sign_in_button;
         private System.Windows.Forms.TextBox Sign_in_pass;
         private System.Windows.Forms.TextBox Sign_in_email;
-        private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
--- Client/Client/Authorization.cs
+++ Client/Client/Authorization.cs
@@ -5,13 +5,16 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Client
 {
     public partial class Authorization : Form
     {
+        
         public Authorization()
         {
             InitializeComponent();
@@ -20,13 +23,43 @@ namespace Client
         #region WorkWithButton
         private async void Sign_in_button_Click(object sender, EventArgs e)
         {
+            buttonClicked = true; // Позначаємо, що кнопка була натиснута
 
+            if (!isEmailValid && buttonClicked)
+            {
+                ShowError2("Уведіть правильну адресу електронної пошти");
+            }
+            else if (!isPasswordValid && buttonClicked)
+            {
+                ShowError2("Пароль має містити мінімум 8 символів, одну велику літеру, одну малу літеру та одну цифру");
+            }
+            else
+            {
+                // Продовжуємо з подальшою логікою реєстрації
+                // Наприклад, збереження даних, виклик API і т.д.
+                label2.Visible = false; // Приховати помилку якщо формат правильний
+            }
         }
 
+        private bool buttonClicked = false;
         private async void Sign_up_button_Click(object sender, EventArgs e)
         {
+            buttonClicked = true; // Позначаємо, що кнопка була натиснута
 
-
+            if (!isEmailValid && buttonClicked)
+            {
+                ShowError("Уведіть правильну адресу електронної пошти");
+            }
+            else if (!isPasswordValid && buttonClicked)
+            {
+                ShowError("Пароль має містити мінімум 8 символів, одну велику літеру, одну малу літеру та одну цифру");
+            }
+            else
+            {
+                // Продовжуємо з подальшою логікою реєстрації
+                // Наприклад, збереження даних, виклик API і т.д.
+                label1.Visible = false; // Приховати помилку якщо формат правильний
+            }
         }
 
         #endregion
@@ -35,13 +68,34 @@ namespace Client
 
         private async void Sign_up_pass2_TextChanged(object sender, EventArgs e)
         {
+            string password1 = Sign_up_pass.Text;
+            string password2 = Sign_up_pass2.Text;
 
+            if (password1 == password2)
+            {
+                // Паролі співпадають
+                // Можна виконати додаткові дії, наприклад, приховати помилку про неправильний повтор пароля
+                label2.Visible = false; // Приховуємо помилку, якщо паролі співпадають
+            }
+            else
+            {
+                ShowError("Пароль не співпадає");
+            }
         }
-
+        private bool isPasswordValid = false;
         private async void Sign_up_pass_TextChanged(object sender, EventArgs e)
         {
+            string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$";
+            isPasswordValid = Regex.IsMatch(Sign_up_pass.Text, passwordPattern);
 
-
+            if (buttonClicked && !isPasswordValid)
+            {
+                ShowError("Пароль має містити мінімум 8 символів, одну велику літеру, одну малу літеру та одну цифру");
+            }
+            else
+            {
+                label1.Visible = false; // Приховуємо помилку, якщо пароль валідний
+            }
         }
 
         private async void Sign_in_pass_TextChanged(object sender, EventArgs e)
@@ -54,14 +108,103 @@ namespace Client
         #region WorkWithEmail
         private async void Sign_in_email_TextChanged(object sender, EventArgs e)
         {
+            string emailPattern = @"^[^@\s]+@[^\s@]+\.[^\s@]+$";
+            isEmailValid = Regex.IsMatch(Sign_up_email.Text, emailPattern);
 
+            if (buttonClicked && !isEmailValid)
+            {
+                ShowError2("Уведіть правильну адресу електронної пошти");
+            }
+            else
+            {
+                label2.Visible = false; // Приховуємо помилку, якщо емейл валідний
+            }
         }
 
+
+
+        private bool isEmailValid = false;
         private async void Sign_up_email_TextChanged(object sender, EventArgs e)
         {
+            string emailPattern = @"^[^@\s]+@[^\s@]+\.[^\s@]+$";
+            isEmailValid = Regex.IsMatch(Sign_up_email.Text, emailPattern);
+
+            if (buttonClicked && !isEmailValid)
+            {
+                ShowError("Уведіть правильну адресу електронної пошти");
+            }
+            else
+            {
+                label1.Visible = false; // Приховуємо помилку, якщо емейл валідний
+            }
+
+
 
         }
 
         #endregion
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Якщо чекбокс позначений, властивість UseSystemPasswordChar вимкнена
+            if (checkBox1.Checked)
+            {
+                Sign_up_pass.UseSystemPasswordChar = false;
+                Sign_up_pass2.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                // Якщо чекбокс не позначений, властивість UseSystemPasswordChar увімкнена
+                Sign_up_pass.UseSystemPasswordChar = true;
+                Sign_up_pass2.UseSystemPasswordChar = true;
+            }
+        }
+
+
+        private void ShowError(string message)
+        {
+            label1.Text = message;
+            label1.ForeColor = Color.Red;
+            label1.Visible = true;
+        }
+        private void ShowError2(string message)
+        {
+            label2.Text = message;
+            label2.ForeColor = Color.Red;
+            label2.Visible = true;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Уведіть правильну адресу електронної пошти";
+            label1.ForeColor = Color.Red;
+            label1.Visible = true;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            // Якщо чекбокс позначений, властивість UseSystemPasswordChar вимкнена
+            if (checkBox2.Checked)
+            {
+                Sign_in_pass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                // Якщо чекбокс не позначений, властивість UseSystemPasswordChar увімкнена
+                Sign_in_pass.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
