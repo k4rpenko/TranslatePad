f0088fd3fb040011866adfb8e125bb5f86b69fb6
Dima1385 <Lozik1385@gmail.com>
2024-06-28 20:26:25 +0300

    add translate post

--- Client/Client/api/send.cs
+++ Client/Client/api/send.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Client.api
 {
@@ -25,6 +26,31 @@ namespace Client.api
             {
                 var data = new { email = email, password = password };
                 HttpResponseMessage response = await _client.PostAsJsonAsync(url, data);
+                return response;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"HTTP POST request failed: {ex.Message}");
+                return null;
+            }
+
+        }
+
+        public async Task<HttpResponseMessage> PostTranslate(string text, string textLenght)
+        {
+            string apiKey = "your-api-key";
+            string url = "https:\\api.deepl.com\v2\translate";
+
+            try
+            {
+                var values = new Dictionary<string, string>
+                {
+                     { "auth_key", apiKey },
+                     { "text", text },
+                     { "target_lang", textLenght }
+                };
+
+                HttpResponseMessage response = await _client.PostAsJsonAsync(url, values);
                 return response;
             }
             catch (HttpRequestException ex)
