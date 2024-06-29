73e39a9b1993f6c91dda2862e1739ed08251d909
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-06-29 15:49:16 +0300

    final beckend

--- Client/Client/Test.Designer.cs
+++ Client/Client/Test.Designer.cs
@@ -34,11 +34,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label_test = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(365, 300);
+            this.button1.Location = new System.Drawing.Point(365, 368);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(106, 35);
             this.button1.TabIndex = 0;
@@ -48,7 +49,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(265, 228);
+            this.textBox1.Location = new System.Drawing.Point(265, 296);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(300, 35);
@@ -56,7 +57,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(265, 178);
+            this.textBox2.Location = new System.Drawing.Point(265, 246);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(300, 35);
@@ -64,7 +65,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(594, 176);
+            this.button2.Location = new System.Drawing.Point(594, 244);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(60, 37);
             this.button2.TabIndex = 3;
@@ -74,7 +75,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(594, 228);
+            this.button3.Location = new System.Drawing.Point(594, 296);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(60, 35);
             this.button3.TabIndex = 4;
@@ -85,16 +86,26 @@
             // label_test
             // 
             this.label_test.AutoSize = true;
-            this.label_test.Location = new System.Drawing.Point(265, 112);
+            this.label_test.Location = new System.Drawing.Point(262, 221);
             this.label_test.Name = "label_test";
             this.label_test.Size = new System.Drawing.Size(0, 13);
             this.label_test.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(265, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(389, 173);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label_test);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -116,5 +127,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label_test;
+        private System.Windows.Forms.ListView listView1;
     }
 }
