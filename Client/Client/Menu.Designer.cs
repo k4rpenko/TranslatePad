9be86251af1dadcf06c73a636640e273add8f06c
Dima1385 <Lozik1385@gmail.com>
2024-06-30 22:47:13 +0300

    uptade Dictionary button

--- Client/Client/Menu.Designer.cs
+++ Client/Client/Menu.Designer.cs
@@ -33,9 +33,6 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
@@ -68,15 +65,13 @@
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
             this.panel3.Controls.Add(this.pictureBox3);
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.button5);
-            this.panel3.Controls.Add(this.button4);
-            this.panel3.Controls.Add(this.button3);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel3.Location = new System.Drawing.Point(199, 0);
             this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(805, 611);
             this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
             // pictureBox3
             // 
@@ -100,49 +95,6 @@
             this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Нещодавні зміни";
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
-            this.button5.Location = new System.Drawing.Point(244, 46);
-            this.button5.Margin = new System.Windows.Forms.Padding(2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(82, 85);
-            this.button5.TabIndex = 2;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
-            this.button4.Location = new System.Drawing.Point(144, 46);
-            this.button4.Margin = new System.Windows.Forms.Padding(2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(82, 85);
-            this.button4.TabIndex = 1;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
-            this.button3.Location = new System.Drawing.Point(44, 46);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(82, 85);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // panel2
             // 
@@ -269,9 +221,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
--- Client/Client/Menu.cs
+++ Client/Client/Menu.cs
@@ -1,22 +1,75 @@
-﻿using System;
+﻿using Client.api;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace Client
 {
     public partial class Menu : Form
     {
         FormProfile _FP = new FormProfile();
+
+
+        private class Notes
+        {
+            public int id { get; set; }
+            public int user_id { get; set; }
+            public string title { get; set; }
+            public string content { get; set; }
+            public string updated_at { get; set; }
+        }
+
+        private int buttonCounter = 0;
+        private int startX = 44; // Початкова позиція по X
+        private int startY = 46; // Початкова позиція по Y
+        private int buttonWidth = 85; // Ширина кнопки
+        private int buttonHeight = 85; // Висота кнопки
+        private int spacing = 10; // Відстань між кнопками
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+        private static string RefreshFilePath = "user_refresh.txt";
+        string token = File.ReadAllText(RefreshFilePath);
+        Http_Send httpSend = new Http_Send();
+
+
+
         public Menu()
         {
             InitializeComponent();
+            showDictionary();
             this.FormClosed += new FormClosedEventHandler(Menu_FormClosed);
+        }
+
+        private async void showDictionary()
+        {
+            try
+            {
+                string url = "http://localhost:3001/api/Show_Notes";
+                HttpResponseMessage response = await httpSend.GetShowNotes(url, token);
+                if ((int)response.StatusCode == 200)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    List<Notes> translations = JsonConvert.DeserializeObject<List<Notes>>(jsonResponse);
+                    int a = translations.Count;
+                    Console.WriteLine(translations.Count);
+                    for (int i = 0; i < a; i++)
+                    {
+                        GenerateElements(translations[i].id, translations[i].title.ToString());
+                    }
+                    this.Refresh();
+                }
+                else { Console.WriteLine("NULL"); }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
         }
 
         private void Menu_FormClosed(object sender, FormClosedEventArgs e)
@@ -79,5 +132,63 @@ namespace Client
                 _FP.Focus();
             }
         }
+
+
+        private void GenerateElements(int id, string title)
+        {
+            Console.WriteLine("Start create");
+            // Створюємо Button
+
+            //CreatePictureBox(id);
+            CreateButton(id, title);
+        }
+
+
+        private void CreatePictureBox(int index)
+        {
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox.Image = Image.FromFile("free-icon-font-file.png"); // Замініть шлях на реальний шлях до вашого зображення
+            pictureBox.Location = new Point((this.ClientSize.Width - pictureBox.Width) / 2, (this.ClientSize.Height - pictureBox.Height) / 2);
+            panel2.Controls.Add(pictureBox);
+        }
+
+        private void CreateButton(int index, string text)
+        {
+            Button newButton = new Button();
+            newButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            newButton.FlatStyle = FlatStyle.Popup;
+            newButton.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            newButton.ForeColor = Color.FromArgb(140, 140, 140);
+            newButton.Margin = new Padding(2);
+            newButton.Name = $"dynamicButton{index}";
+            newButton.Size = new Size(buttonWidth, buttonHeight);
+            newButton.Text = text;
+
+
+            // Розрахунок позиції нової кнопки
+            int x = startX + (buttonCounter * (buttonWidth + spacing));
+            int y = startY;
+
+            // Якщо кнопка не вміщується в ширину форми, переносимо на наступний рядок
+            if (x + buttonWidth > this.ClientSize.Width)
+            {
+                buttonCounter = 0;
+                startY += buttonHeight + spacing;
+                x = startX + (buttonCounter * (buttonWidth + spacing));
+                y = startY;
+            }
+
+            newButton.Location = new Point(x, y);
+            panel3.Controls.Add(newButton);
+            buttonCounter++;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
+
+
 }
