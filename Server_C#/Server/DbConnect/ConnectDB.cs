26820064919154d93ac7a37a88950ffd5d010b34
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-07-10 17:26:38 +0300

    test

---
+++ Server_C#/Server/DbConnect/ConnectDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Npgsql;
+using Microsoft.Extensions.Options;
+
+public class ConnectDB : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public ConnectDB(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConnectAndQuery(Action<NpgsqlConnection> queryAction)
+    {
+        try
+        {
+            using (var connection = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                queryAction(connection);
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
