97d0a2b809993c85cd801176fae7c24003737c64
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-07-12 15:56:59 +0300

    Make db in C#

---
+++ Server_C/Server/Methods/Hash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Server.Methods;
+
+public class Hash
+{
+    public string password { get; set; }
+
+    public Hash(string password)
+    {
+        this.password = password;
+    }
+
+    public string HashPassword256()
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+            byte[] hash = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
---
+++ Server_C/Server/Methods/tokenService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+namespace Server.Methods;
+
+public class tokenService
+{
+    private static string secretKey = "1c8ba0fa414c97d1cf34d12fd2a14db296bcde4b0a818157413f6a91f104766d";
+    
+    public static string GenerateRefreshToken(string data)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] { new Claim("data", data) }),
+            Expires = DateTime.UtcNow.AddDays(30),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
