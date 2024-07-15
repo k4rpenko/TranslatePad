c92169e8f4213def2d5ee674342e90ce764e6bc2
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-07-15 15:23:33 +0300

    Delete website directory

--- website/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/errorPage.module.css';
-
-export default function NotFound(){
-    return(
-        <div><div className={styles.page}><h1>404</h1></div></div>
-    )
-}
